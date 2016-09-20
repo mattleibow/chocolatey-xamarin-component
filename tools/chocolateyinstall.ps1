d0f517d0dd4eccf938b4a3a39acb02ab7e8d3408
Matthew Leibowitz <mattleibow@live.com>
2016-09-20 19:59:31 +0200

    Updated to use NuGet version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = "https://www.nuget.org/api/v2/package/XamarinComponent/$env:chocolateyPackageVersion"
+$checksum     = 'FA3E37B43BF528B09D9D8F04B95969CEC9828C4C87C26273E3E2E731A804FDEE'
+
+$packageArgs = @{
+  packageName    = 'xamarin-component'
+  unzipLocation  = $toolsDir
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
